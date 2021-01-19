21d95e0580458217c003b660f99c63114051334d
tanu320 <60231791+tanu320@users.noreply.github.com>
2021-01-19 12:22:47 +0530

    Changes the color schemes and other stuff

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -21,59 +21,55 @@ class Home extends StatelessWidget {
               child: Column(children: <Widget>[
            //     SizedBox(height: 5,),
             Container(
-              color: Colors.blue[900],
+                color: Colors.deepPurple[900],
                 height: queryData.orientation == Orientation.portrait
                     ? queryData.size.height * 0.1
                     : queryData.size.height * 0.15,
-                child: Card(
-                  color: Colors.blue[900],
-                    elevation: 0,
-                    child: Row(children: <Widget>[
-                      Builder(
-                          builder: (context) => IconButton(
-                                icon: Icon(Icons.menu,color: Colors.white,),
+                child: Row(children: <Widget>[
+                  Builder(
+                      builder: (context) => IconButton(
+                            icon: Icon(Icons.menu,color: Colors.white,),
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                            },
+                          )),
+                  Card(
+                      elevation: 0,
+                      color: Colors.grey[200],
+                      child: Container(
+                          width: queryData.size.width * 0.65,
+                          child: Row(children: <Widget>[
+                            FlatButton(
                                 onPressed: () {
-                                  Scaffold.of(context).openDrawer();
+                                  Navigator.of(context)
+                                      .pushNamed(SearchScreen.routeName);
                                 },
-                              )),
-                      Card(
-                          elevation: 0,
-                          color: Colors.grey[200],
-                          child: Container(
-                              width: queryData.size.width * 0.65,
-                              child: Row(children: <Widget>[
-                                FlatButton(
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .pushNamed(SearchScreen.routeName);
-                                    },
-                                    child: Row(children: <Widget>[
-                                      Icon(Icons.search),
-                                      SizedBox(width: 5,),
-                                      Text('Search',style: TextStyle(color: Colors.grey[600]),),
-                                      Padding(
-                                          padding: EdgeInsets.only(
-                                        right: queryData.size.width * 0.65 * 0.4,
-                                      ))
-                                    ]))
-                              ]))),
-                      
-                      Padding(
-                        padding: const EdgeInsets.only(left:8),
-                        child: Card(
-                            elevation: 0,
-                            color: Colors.blue[900],
-                            child: IconButton(
-                              icon: Icon(Icons.message,
-                              size: 30,
-                              color: Colors.white,),
-                              onPressed: () {
-                                //    ChatScreen();
-                              },
-                            )),
-                      )
-                    ])
-                    ),
+                                child: Row(children: <Widget>[
+                                  Icon(Icons.search),
+                                  SizedBox(width: 5,),
+                                  Text('Search',style: TextStyle(color: Colors.grey[600]),),
+                                  Padding(
+                                      padding: EdgeInsets.only(
+                                    right: queryData.size.width * 0.65 * 0.4,
+                                  ))
+                                ]))
+                          ]))),
+                  
+                  Padding(
+                    padding: const EdgeInsets.only(left:8),
+                    child: Card(
+                        elevation: 0,
+                        color: Colors.deepPurple[900],
+                        child: IconButton(
+                          icon: Icon(Icons.message,
+                          size: 30,
+                          color: Colors.white,),
+                          onPressed: () {
+                            //    ChatScreen();
+                          },
+                        )),
+                  )
+                ]),
                     ),
                   HomeTab()
           ]))),
